7555cac9943601bc51330c34924293e64826e132
zhongbo320104 <3279028951@qq.com>
2021-11-25 23:30:36 +0800

    删除ignore

---
+++ react-app/src/components/containerComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class containerComponent extends Component {
+    constructor(props) {
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
